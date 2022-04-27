c9859b22be49522733c4676de9eec7fba9f18fa7
Summer Mousa <3171936+smousa@users.noreply.github.com>
2022-04-27 07:56:00 -0500

    added tests for GA helper fns

---
+++ gaboom/src/test/java/org/omegabyte/gaboom/transforms/ga/MapPopulationFnTest.java
@@ -0,0 +1,25 @@
+package org.omegabyte.gaboom.transforms.ga;
+
+import org.apache.beam.sdk.transforms.DoFnTester;
+import org.apache.beam.sdk.values.KV;
+import org.junit.Test;
+import org.omegabyte.gaboom.Individual;
+import org.omegabyte.gaboom.Population;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MapPopulationFnTest {
+
+    @Test
+    // It should map populations by their id
+    public void testMapPopulationFn() throws Exception {
+        Population<Integer> population = new Population<>("testpop");
+
+        DoFnTester<Population<Integer>, KV<String, Population<Integer>>> fnTester = DoFnTester.of(new MapPopulationFn<>());
+        assertEquals(KV.of(population.getId(), population), fnTester.processBundle(population).get(0));
+    }
+
+}
