8afc64c3d374d3197ecc3c5a43ddcffb37825a17
Summer Mousa <3171936+smousa@users.noreply.github.com>
2022-04-27 07:56:01 -0500

    added test for model down to size

---
+++ gaboom/src/test/java/org/omegabyte/gaboom/transforms/model/ModelDownToSizeTest.java
@@ -0,0 +1,123 @@
+package org.omegabyte.gaboom.transforms.model;
+
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SerializableFunction;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.omegabyte.gaboom.BaseItem;
+import org.omegabyte.gaboom.Individual;
+import org.omegabyte.gaboom.Individuals;
+import org.omegabyte.gaboom.SelectIndividuals;
+import org.omegabyte.gaboom.transforms.Crossover;
+import org.omegabyte.gaboom.transforms.Evaluate;
+import org.omegabyte.gaboom.transforms.Mutate;
+import org.omegabyte.gaboom.transforms.Populate;
+import org.omegabyte.gaboom.transforms.select.SelectRouletteFn;
+import org.omegabyte.gaboom.transforms.select.SelectTournamentFn;
+
+import java.io.Serializable;
+
+import static org.junit.Assert.*;
+
+public class ModelDownToSizeTest {
+    private static final int seed = 0;
+    private PCollection<KV<String, Individuals<String>>> input;
+
+    static class ToKVIndividuals extends DoFn<Individuals<String>, KV<String, Individuals<String>>> implements Serializable {
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+            c.output(KV.of("test", c.element()));
+        }
+    }
+
+    @Rule
+    public transient TestPipeline pipeline = TestPipeline.create();
+
+    @Before
+    public void setUpTest() {
+        ModelTest.SECRET_WORD = "animosity";
+        input = pipeline.apply(Create.of(new BaseItem(seed)))
+                .apply(Populate.as(new ModelTest.PopulateFn()).withPopSize(10))
+                .apply(ParDo.of(new ToKVIndividuals()))
+                .apply(Evaluate.as(new ModelTest.FitnessTransform()));
+    }
+
+    @Test
+    public void testModelDownToSize() {
+        PCollection<KV<String, Individuals<String>>> output = input.apply(new ModelDownToSize<>(
+                new SelectTournamentFn<>(3),
+                new SelectRouletteFn<>(),
+                Crossover.as(new ModelTest.CrossoverFn()).withCrossRate(0.5),
+                Mutate.as(new ModelTest.MutateFn(3)).withMutRate(0.5),
+                new ModelTest.FitnessTransform(),
+                4
+        ));
+        PAssert.that(output).satisfies((SerializableFunction<Iterable<KV<String, Individuals<String>>>, Void>) kvs -> {
+            KV<String, Individuals<String>> result = kvs.iterator().next();
+            assertEquals("test", result.getKey());
+            assertEquals(10, result.getValue().getIndividuals().size());
+
+            Individual<String> ind;
+
+            ind = result.getValue().getIndividuals().get(0);
+            assertEquals("KNrHVY", ind.getId());
+            assertEquals("egqflaqlo", ind.getGenome());
+            assertEquals((Double) 73.0, ind.getFitness());
+
+            ind = result.getValue().getIndividuals().get(1);
+            assertEquals("l7V350", ind.getId());
+            assertEquals("poqflsoly", ind.getGenome());
+            assertEquals((Double) 48.0, ind.getFitness());
+
+            ind = result.getValue().getIndividuals().get(2);
+            assertEquals("l7V350", ind.getId());
+            assertEquals("poqflsoly", ind.getGenome());
+            assertEquals((Double) 48.0, ind.getFitness());
+
+            ind = result.getValue().getIndividuals().get(3);
+            assertEquals("RJF1Sj", ind.getId());
+            assertEquals("cbosyqqkc", ind.getGenome());
+            assertEquals((Double) 77.0, ind.getFitness());
+
+            ind = result.getValue().getIndividuals().get(4);
+            assertEquals("n6nDPq", ind.getId());
+            assertEquals("powflsomo", ind.getGenome());
+            assertEquals((Double) 63.0, ind.getFitness());
+
+            ind = result.getValue().getIndividuals().get(5);
+            assertEquals("7vOGJI", ind.getId());
+            assertEquals("zxkgxtsps", ind.getGenome());
+            assertEquals((Double) 73.0, ind.getFitness());
+
+            ind = result.getValue().getIndividuals().get(6);
+            assertEquals("cPNtYF", ind.getId());
+            assertEquals("wmrlfxblg", ind.getGenome());
+            assertEquals((Double) 80.0, ind.getFitness());
+
+            ind = result.getValue().getIndividuals().get(7);
+            assertEquals("l7V350", ind.getId());
+            assertEquals("poqflsoly", ind.getGenome());
+            assertEquals((Double) 48.0, ind.getFitness());
+
+            ind = result.getValue().getIndividuals().get(8);
+            assertEquals("KNrHVY", ind.getId());
+            assertEquals("egqflaqlo", ind.getGenome());
+            assertEquals((Double) 73.0, ind.getFitness());
+
+            ind = result.getValue().getIndividuals().get(9);
+            assertEquals("n6nDPq", ind.getId());
+            assertEquals("powflsomo", ind.getGenome());
+            assertEquals((Double) 63.0, ind.getFitness());
+            return null;
+        });
+        pipeline.run();
+    }
+
+}
