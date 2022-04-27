82536176969518675d229e0d91cd15b6d35eff45
Summer Mousa <3171936+smousa@users.noreply.github.com>
2022-04-27 07:56:00 -0500

    additional test to verify dynamic length for selector

--- gaboom/src/test/java/org/omegabyte/gaboom/transforms/model/IndividualsToSelectorFnTest.java
+++ gaboom/src/test/java/org/omegabyte/gaboom/transforms/model/IndividualsToSelectorFnTest.java
@@ -44,4 +44,33 @@ public class IndividualsToSelectorFnTest {
         Assert.assertEquals(expected.get(0).getValue().getSeed(), actual.get(0).getValue().getSeed());
     }
 
+    @Test
+    // It should set the selector size to length if n is not set
+    public void testAllIndividualsToSelectorFn() throws Exception {
+        List<Individual<Integer>> individualsList = new ArrayList<>();
+        Individual<Integer> individual;
+        individual = new Individual<>("a", 1);
+        individual.setFitness(0.1);
+        individualsList.add(individual);
+        individual = new Individual<>("b", 2);
+        individual.setFitness(0.3);
+        individualsList.add(individual);
+
+
+        List<KV<String, Individuals<Integer>>> input = new ArrayList<>();
+        input.add(KV.of("test", new Individuals<>(20, individualsList)));
+
+        List<KV<String, SelectIndividuals<Integer>>> expected = new ArrayList<>();
+        expected.add(KV.of("test", new SelectIndividuals<>(20, individualsList, 1)));
+
+        DoFnTester<KV<String, Individuals<Integer>>, KV<String, SelectIndividuals<Integer>>> fnTester = DoFnTester.of(new IndividualsToSelectorFn<>());
+        List<KV<String, SelectIndividuals<Integer>>> actual = fnTester.processBundle(input);
+        assert actual.size() == 1;
+        Assert.assertEquals(expected.get(0).getKey(), actual.get(0).getKey());
+        Assert.assertEquals(expected.get(0).getValue().getIndividuals(), actual.get(0).getValue().getIndividuals());
+        Assert.assertEquals(2, actual.get(0).getValue().getN());
+        Assert.assertEquals(expected.get(0).getValue().getSeed(), actual.get(0).getValue().getSeed());
+
+    }
+
 }
